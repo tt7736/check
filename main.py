957dd0becea6596b5c1d3360a2dec7ef85899f53
Tobias Paul Trimmal <tobias.trimmal@student.tugraz.at>
2025-04-30 16:50:29 +0200

    new

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ def translate_english(event):
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
-    output_div.innerText = "Du Hodenkobolt"
+    output_div.innerText = english
     
