545cc92e0a0afba7b5ae188d37bd64c0a5cd72a7
Tobias Paul Trimmal <tobias.trimmal@student.tugraz.at>
2025-05-05 16:21:15 +0200

    new DB10.0

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from pyscript import document
+#from pyscript import document
 
 # from dotenv import load_dotenv
 # load_dotenv()
 #import os
 import supabase
-url = "https://hmiyzverkplczwioalal.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtaXl6dmVya3BsY3p3aW9hbGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNzk5MDgsImV4cCI6MjA2MTk1NTkwOH0.3uRdyRC8A_q9Mk7InN2CAtLSEGd7ffj7C9TNd4shEnA"
-db = supabase.create_client(url, key)
-
 
 def translate_english(event):
+    url = "https://hmiyzverkplczwioalal.supabase.co"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtaXl6dmVya3BsY3p3aW9hbGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNzk5MDgsImV4cCI6MjA2MTk1NTkwOH0.3uRdyRC8A_q9Mk7InN2CAtLSEGd7ffj7C9TNd4shEnA"
+    db = supabase.create_client(url, key)
     data = db.table("todos").insert({"name":"Todo 5"}).execute()
     # input_text = document.querySelector("#english")
     # english = input_text.value
