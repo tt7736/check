5b47852ac83f8e7573f8ce99b53db74d2d0f1ed8
Tobias Paul Trimmal <tobias.trimmal@student.tugraz.at>
2025-05-05 15:54:11 +0200

    new DB8.0

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from pyscript import document
 # from dotenv import load_dotenv
 # load_dotenv()
 #import os
+import supabase
+url = "https://hmiyzverkplczwioalal.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtaXl6dmVya3BsY3p3aW9hbGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNzk5MDgsImV4cCI6MjA2MTk1NTkwOH0.3uRdyRC8A_q9Mk7InN2CAtLSEGd7ffj7C9TNd4shEnA"
+db = supabase.create_client(url, key)
 
 
 def translate_english(event):
@@ -12,10 +16,6 @@ def translate_english(event):
     output_div = document.querySelector("#output")
     if english=="114458":
         output_div.innerText = "Hallo, du bist eingeloggt!"
-        import supabase
-        url = "https://hmiyzverkplczwioalal.supabase.co"
-        key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtaXl6dmVya3BsY3p3aW9hbGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNzk5MDgsImV4cCI6MjA2MTk1NTkwOH0.3uRdyRC8A_q9Mk7InN2CAtLSEGd7ffj7C9TNd4shEnA"
-        db = supabase.create_client(url, key)
         data = db.table("todos").insert(english).execute()
     else:
         output_div.innerText = "Falsch, du nudde!"
