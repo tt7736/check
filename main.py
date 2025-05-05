b832a34d07bf4a42884218d1d5b25da6c61d45b5
Tobias Paul Trimmal <tobias.trimmal@student.tugraz.at>
2025-05-05 12:46:24 +0200

    third try DB

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from pyscript import document
 # load_dotenv()
 
 #import os
-from supabase import create_client
-
-url = "https://hmiyzverkplczwioalal.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtaXl6dmVya3BsY3p3aW9hbGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNzk5MDgsImV4cCI6MjA2MTk1NTkwOH0.3uRdyRC8A_q9Mk7InN2CAtLSEGd7ffj7C9TNd4shEnA"
-supabase = create_client(url, key)
 
 
 def translate_english(event):
+    from supabase import create_client
+
+    url = "https://hmiyzverkplczwioalal.supabase.co"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtaXl6dmVya3BsY3p3aW9hbGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNzk5MDgsImV4cCI6MjA2MTk1NTkwOH0.3uRdyRC8A_q9Mk7InN2CAtLSEGd7ffj7C9TNd4shEnA"
+    supabase = create_client(url, key)
+    
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
