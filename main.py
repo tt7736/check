a5e3904640ffe938cbf87f5b061a3074b38c3dc9
Tobias Paul Trimmal <tobias.trimmal@student.tugraz.at>
2025-05-05 16:33:39 +0200

    new DB10.0

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
+import arrr
 from pyscript import document
 
-# from dotenv import load_dotenv
-# load_dotenv()
-#import os
-#import supabase
 
 def translate_english(event):
-    #url = "https://hmiyzverkplczwioalal.supabase.co"
-    #key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtaXl6dmVya3BsY3p3aW9hbGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNzk5MDgsImV4cCI6MjA2MTk1NTkwOH0.3uRdyRC8A_q9Mk7InN2CAtLSEGd7ffj7C9TNd4shEnA"
-    #db = supabase.create_client(url, key)
-    #data = db.table("todos").insert({"name":"Todo 5"}).execute()
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
-    if english=="114458":
-        output_div.innerText = "Hallo, du bist eingeloggt!"
-        #data = db.table("todos").insert({"name":english}).execute()
-    else:
-        output_div.innerText = "Falsch, du nudde!"
+    output_div.innerText = arrr.translate(english)
